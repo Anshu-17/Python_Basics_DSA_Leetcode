b97329ab8edb53d9eaf81e653aeccd661c2fab3a
Anshuman <anshurp007@gmail.com>
2026-07-28 16:25:36 -0500

    linked list palindrome detection and display functionality

---
+++ LeetCode/Leetcode 234 Linkedlist Palindrome.py
@@ -0,0 +1,53 @@
+
+#Pre-Code Solution Setup
+class singlynode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+        
+    def __str__(self):
+        return str(self.val)
+    
+# createing a linked list
+def create_linked_list(arr):
+    """Automatically creates and connects nodes from a list of values."""
+    if not arr:
+        return None
+    # Creates the head node of the linked list with the first value 
+    head = singlynode(arr[0])
+    current = head
+    
+     # 2. Loop through the rest of the values and link them automatically
+    for val in arr[1:]:
+        current.next = singlynode(val) #create and connect the nextnode
+        current = current.next #Move our pointerto next node
+    return head
+
+# creating a display funtion
+def displaylinkedlist(head):
+    current = head
+    element = []
+    while current:
+        element.append(str(current.val))
+        current =  current.next
+    print(' --> '.join(element))
+    
+
+
+def LinkedlistPalindrome(nums):
+    current = head
+    element_list = []
+    while current:
+        element_list.append(current.val)
+        current = current.next
+    return element_list == element_list[::-1]
+
+    
+head = create_linked_list([1,3,4,7,9,2,5])
+displaylinkedlist(head)
+print(LinkedlistPalindrome(head))
+
+
+head = create_linked_list([1,2,3,3,2,1])
+displaylinkedlist(head)
+print(LinkedlistPalindrome(head))
