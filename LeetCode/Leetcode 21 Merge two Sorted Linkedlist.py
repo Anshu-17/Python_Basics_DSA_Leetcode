6d1a9d3ca04ad2e3a8c48e98870260cdba7b0587
Anshuman <anshurp007@gmail.com>
2026-08-03 17:05:29 -0500

    Refactor mergeTwoLists function to simplify current pointer assignment

--- LeetCode/Leetcode 21 Merge two Sorted Linkedlist.py
+++ LeetCode/Leetcode 21 Merge two Sorted Linkedlist.py
@@ -44,12 +44,11 @@ def mergeTwoLists(list1, list2):
     while list1 and list2:
         if list1.val < list2.val:
             current.next = list1
-            current = list1
             list1 = list1.next
         else:
             current.next = list2
-            current = list2
             list2 = list2.next
+        current = current.next      
     current.next = list1 if list1 else list2
     return dummy.next
 
